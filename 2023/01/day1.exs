f597e7b257b9018980cbab2ae3ce439c7b3f3663
Raffo <raffaello.martini@gmail.com>
2024-11-30 17:07:36 +0000

    day 1 part 1 done

--- 2023/01/day1.exs
+++ 2023/01/day1.exs
@@ -1,8 +1,10 @@
 
-calibration_value = fn list -> String.to_integer(List.first(list)) * 10 + String.to_integer(List.last(list)) end
-File.stream!("input.txt") |> Stream.map(&String.trim/1) |> (&Regex.scan(~r/\D*(\d).*(\d)\D*/, &1)).() |> List.flatten |> &calibration_value.()
-
-
-
-# That ugly thing is an anonymous function, it is required because Regex.run first argument is the regex, not the input.
-# "5knjbxgvhktvfcq89onefive" |> (&Regex.scan(~r/\D*(\d).*(\d)\D*/, &1)).() |> List.flatten
+calibration_value = fn (line) ->
+    digits = Regex.scan(~r/\d/, line) |> List.flatten
+    [List.first(digits), List.last(digits)]
+    |> Enum.map(fn string -> String.to_integer(string) end)
+    |> (fn list -> List.first(list) * 10 + List.last(list) end).()
+end
+File.stream!("input.txt") |>
+Enum.map(calibration_value) |>
+Enum.sum()
